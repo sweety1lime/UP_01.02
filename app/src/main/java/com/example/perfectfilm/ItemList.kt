ccd9a321b9ebf39435b429f6c3e74c882025706b
sweety1lime <sweety.lime@bk.ru>
2020-11-20 12:34:49 +0300

    Осталось добавить выгрузку ожидаемых фильмов и анимацию

--- app/src/main/java/com/example/perfectfilm/ItemList.kt
+++ app/src/main/java/com/example/perfectfilm/ItemList.kt
@@ -4,18 +4,47 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.perfectfilm.Model.Json_results
 import com.example.perfectfilm.Model.ViewModel
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
 
+const val  URI_IMAGE : String = "https://image.tmdb.org/t/p/w500/"
 class ItemList(
     private val context: Context,
-    private val movie: List<ViewModel>,
-    val listener: (ViewModel) -> Unit
+    private val movie: ArrayList<Json_results>?,
+    val listener: (Json_results) -> Unit
 ) : RecyclerView.Adapter<ItemList.ImageViewHolder>() {
 
     class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val rating = view.findViewById<TextView>(R.id.vote_average)
+        val poster = view.findViewById<ImageView>(R.id.poster)
+        var title = view.findViewById<TextView>(R.id.title)
+        var progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
+        fun View(date: Json_results, context: Context, listener: (Json_results) -> Unit) {
+            rating.text = date.vote_average + "/10"
+            title.text = date.title
+
+
+            Picasso.with(context)
+                .load(URI_IMAGE + date.poster_path)
+                .into(poster, object : Callback {
+                    override fun onSuccess() {
+                        if (progressBar != null) {
+                            progressBar.visibility = View.GONE
+                        }
+                    }
+
+                    override fun onError() {
+                        TODO("Not yet implemented")
+                    }
+                })
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder =
@@ -23,9 +52,9 @@ class ItemList(
             LayoutInflater.from(context).inflate(R.layout.item_of_list, parent, false)
         )
 
-    override fun getItemCount(): Int = movie.size
+    override fun getItemCount(): Int = movie!!.size
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.View(movie?.get(position)!!, this.context, listener)
     }
-
 }
--- app/src/main/java/com/example/perfectfilm/Top_Film.kt
+++ app/src/main/java/com/example/perfectfilm/Top_Film.kt
@@ -1,39 +1,41 @@
 package com.example.perfectfilm
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.perfectfilm.Model.ViewModel
+import kotlinx.android.synthetic.main.fragment_top__film.*
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+const val  URI : String = "https://api.themoviedb.org/3/movie/"
 
 class Top_Film : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sendNetvorkReauest()
+
+
+
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_expected__film, container, false)
+        return inflater.inflate(R.layout.fragment_top__film, container, false)
     }
 
     fun sendNetvorkReauest()
     {
-        var const = Const()
-        val builder = Retrofit.Builder().baseUrl(const.URL).addConverterFactory(GsonConverterFactory.create())
+        val builder = Retrofit.Builder().baseUrl(URI).addConverterFactory(GsonConverterFactory.create())
         val retrofit= builder.build()
         val interfase = retrofit.create<Interface>(Interface::class.java)
 
@@ -42,14 +44,27 @@ class Top_Film : Fragment() {
         call.enqueue(object : retrofit2.Callback<ViewModel>
         {
             override fun onFailure(call: Call<ViewModel>, t: Throwable) {
-
+                TODO("Not yet implemented")
             }
 
             override fun onResponse(call: Call<ViewModel>, response: Response<ViewModel>) {
+                var array = response.body()!!.result
+                activity?.runOnUiThread () {
 
-                Log.i("Test",  response.body()!!.result!!.get(0).title!!)
+                    val recycler = activity?.findViewById<RecyclerView>(R.id.imageRecyclerView)
+                    recycler?.layoutManager = LinearLayoutManager(activity)
+                    recycler?.setHasFixedSize(true)
+
+                    val adapter: ItemList = ItemList(activity!!.applicationContext, array)
+                    {
+
+                    }
+                    recycler?.adapter = adapter
+                }
             }
 
         })
+
+
     }
 }
